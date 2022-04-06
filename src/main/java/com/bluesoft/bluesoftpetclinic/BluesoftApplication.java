8d918fa6a1197b6c01e4a6d24c6a99e575bd6cff
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-04-06 15:06:48 +0200

    Initial commit

---
+++ src/main/java/com/bluesoft/bluesoftpetclinic/BluesoftApplication.java
@@ -0,0 +1,13 @@
+package com.bluesoft.bluesoftpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluesoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluesoftApplication.class, args);
+    }
+
+}
